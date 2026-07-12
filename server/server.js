f3f45397de41a84b9ba15935b18034fbbf1505fe
hwnest25 <hop.wilson.25@lehre.mosbach.dhbw.de>
2026-07-12 18:41:52 +0200

    debug: log PORT env var on startup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ dotenv.config();
 import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
+console.log(`PORT env var: ${process.env.PORT}`);
+console.log(`Listening on port: ${PORT}`);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`AcadeMe server running on port ${PORT}`);
